9972801f536f3502ed825ebd9285da243be6f4c1
akhilmunnuru26 <munnuruakhil26@gmail.com>
2023-12-07 13:08:25 +0530

    Implemented create/sign-up user using firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
     if (!isSignInForm) {
       //Sign Up Logic
 
-      createUserWithEmailAndPassword(auth, email, password)
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
@@ -37,6 +37,7 @@ const Login = () => {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
+          setErrorMessage(errorCode+"-"+errorMessage)
           // ..
         });
     } else {
